9f19071efdcc2609effdf39d1d6b3bce24be94b1
Fannabykiki <namplhe153292@fpt.edu.vn>
2023-03-16 22:50:51 +0700

    View order

---
+++ app/src/main/java/com/example/holayummy/ViewHolder/OrderViewHolder.java
@@ -0,0 +1,43 @@
+package com.example.holayummy.ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.holayummy.Interface.ItemClickListener;
+import com.example.holayummy.R;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtOrderId, txtOrderStatus, txtOrderPhone,txtOrderAddress;
+    public ItemClickListener itemClickListener;
+
+    private void bindingView() {
+        txtOrderAddress = itemView.findViewById(R.id.order_address);
+        txtOrderId = itemView.findViewById(R.id.order_id);
+        txtOrderPhone = itemView.findViewById(R.id.order_phone);
+        txtOrderStatus = itemView.findViewById(R.id.order_status);
+    }
+    private void bindingAction() {
+        itemView.setOnClickListener(this);
+    }
+    public OrderViewHolder(@NonNull View itemView) {
+        super(itemView);
+        bindingView();
+        bindingAction();
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
+
+    }
+}
